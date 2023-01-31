3aa1af9192693426150e57b30a1968d90adda517
hoangvytb7891 <hoangvytb7891@gmail.com>
2023-01-31 22:21:28 +0700

    trùm regex

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -1,41 +1,31 @@
 package chucknorris;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Main {
 
+    private static final String NORRIS_ENCODED_SEQUENCE_REGEX = "(0{1,2} 0*)";
+    private static final String SEVEN_BIT_CHUNK_REGEX_PATTERN = "(?<=\\G.{7})";
+
     public static void main(String[] args) {
-        System.out.println("Input string: ");
         Scanner sc = new Scanner(System.in);
-        char[] res = sc.nextLine().toCharArray();
-        StringBuilder sb = new StringBuilder();
-        for (char c: res) {
-            String bin = Integer.toBinaryString(c);
-            while (bin.length() < 7) {
-                bin = '0' + bin;
-            }
-            sb.append(bin);
-        }
-        System.out.println();
-        System.out.println("The result: ");
-        int i = 0;
-        char currentChar;
-        while (i < sb.length()) {
-            if (sb.charAt(i) == '0') {
-                System.out.print("00 ");
-                currentChar = '0';
-            }
-            else {
-                System.out.print("0 ");
-                currentChar = '1';
-            }
-            while (sb.charAt(i) == currentChar) {
-                System.out.print("0");
-                i++;
-                if (i >= sb.length()) break;
-            }
-            if (i < sb.length()) System.out.print(" ");
-        }
-        sc.close();
+        System.out.println("Input encoded string: ");
+        String in = sc.nextLine();
+        Pattern pattern = Pattern.compile(NORRIS_ENCODED_SEQUENCE_REGEX);
+        Matcher matcher = pattern.matcher(in);
+        System.out.println("\nThe result: ");
+        Arrays.stream(matcher.results()
+                .map(MatchResult::group)
+                .map(s -> s.split(" "))
+                .map(s -> s[0].equals("0") ? "1".repeat(s[1].length()) : "0".repeat(s[1].length()))
+                .collect(Collectors.joining())
+                .split(SEVEN_BIT_CHUNK_REGEX_PATTERN))
+                .map(s -> (char) Integer.parseInt(s, 2))
+                .forEach(System.out::print);
     }
 }
