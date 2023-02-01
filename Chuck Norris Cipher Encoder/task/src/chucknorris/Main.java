3204574bcbe5e5257142b417f25b1b670466ac63
hoangvytb7891 <hoangvytb7891@gmail.com>
2023-02-01 22:46:02 +0700

    completed stage

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -1,5 +1,6 @@
 package chucknorris;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.MatchResult;
@@ -9,23 +10,200 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    private static final String NORRIS_ENCODED_SEQUENCE_REGEX = "(0{1,2} 0*)";
-    private static final String SEVEN_BIT_CHUNK_REGEX_PATTERN = "(?<=\\G.{7})";
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input encoded string: ");
-        String in = sc.nextLine();
-        Pattern pattern = Pattern.compile(NORRIS_ENCODED_SEQUENCE_REGEX);
-        Matcher matcher = pattern.matcher(in);
-        System.out.println("\nThe result: ");
-        Arrays.stream(matcher.results()
-                .map(MatchResult::group)
-                .map(s -> s.split(" "))
-                .map(s -> s[0].equals("0") ? "1".repeat(s[1].length()) : "0".repeat(s[1].length()))
-                .collect(Collectors.joining())
-                .split(SEVEN_BIT_CHUNK_REGEX_PATTERN))
-                .map(s -> (char) Integer.parseInt(s, 2))
-                .forEach(System.out::print);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Please input operation (encode/decode/exit):");
+            String operation = scanner.nextLine();
+
+            switch (operation) {
+                case "encode" -> {
+                    System.out.println("Input string:");
+                    String inputString = scanner.nextLine();
+                    System.out.printf("Encoded string:\n%s\n\n", convertToCNUCode(convertToBinaryString(inputString)));
+                }
+
+                case "decode" -> {
+                    System.out.println("Input encoded string:");
+                    String encodedString = scanner.nextLine();
+
+                    // Error prevention on the encoded string input.
+                    String errorMessage = "Encoded string is not valid.\n";
+                    String checkForInvalidCharacters = encodedString.replaceAll("\\s", "")
+                            .replaceAll("0", "");
+                    if (checkForInvalidCharacters.length() != 0) {
+                        System.out.println(errorMessage);
+                        continue;
+                    } else if (!isValidFirstBlock(encodedString)) {
+                        System.out.println(errorMessage);
+                        continue;
+                    } else if (!isValidAmountOfBlocks(encodedString)) {
+                        System.out.println(errorMessage);
+                        continue;
+                    } else if (!isValidBinaryStringLength(cnuCodeToBinary(encodedString))) {
+                        System.out.println(errorMessage);
+                        continue;
+                    }
+
+                    System.out.printf("Decoded string:\n%s\n\n", binaryToString(cnuCodeToBinary(encodedString)));
+                }
+
+                case "exit" -> {
+                    System.out.println("Bye!");
+                    System.exit(0);
+                }
+
+                default -> System.out.printf("There is no '%s' operation\n\n", operation);
+            }
+        }
     }
+
+    /**
+     * Checks to see if the binary string that is generated from decoding the chuck norris code is of valid length.
+     *
+     * @param binaryString  the encoded message, in binary form
+     * @return              true if length is valid, false if not
+     */
+    private static boolean isValidBinaryStringLength(String binaryString) {
+        return binaryString.length() % 7 == 0;
+    }
+
+    /**
+     * Checks to see if the chuck norris code is made up of valid amount of blocks.
+     *
+     * @param encodedString  an encoded chuck norris unary code
+     * @return               true if the amount is valid, false if not
+     */
+    private static boolean isValidAmountOfBlocks(String encodedString) {
+        return encodedString.trim().split(" ").length % 2 == 0;
+    }
+
+    /**
+     * Checks if the first block of each sequence is not 0 or 00.
+     *
+     * @param encodedString  an encoded chuck norris unary code
+     * @return               true if the first block of each sequence is valid, false if not
+     */
+    private static boolean isValidFirstBlock(String encodedString) {
+        int whiteSpaceCounter = 0, startingIndex = 0;
+        for (int i = 0; i < encodedString.length(); i++) {
+            if (encodedString.charAt(i) == ' ') {
+                whiteSpaceCounter++;
+            }
+
+            if (whiteSpaceCounter == 2 || i == encodedString.length() - 1) {
+                String[] pair = encodedString.substring(startingIndex, i + 1).trim().split(" ");
+                if (pair[0].length() > 2) {
+                    return false;
+                }
+
+                startingIndex = i;
+                whiteSpaceCounter = 0;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Converts the user input to the binary representation.
+     *
+     * @param input  the message that the user gave
+     * @return       binary representation of the input parameter
+     */
+    private static String convertToBinaryString(String input) {
+        StringBuilder sb = new StringBuilder();
+
+        char[] chars = input.toCharArray();
+        for (char ch : chars) {
+            sb.append(
+                    String.format("%7s", Integer.toBinaryString(ch)).replaceAll(" ", "0")
+            );
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Converts a binary string to chuck norris unary code.
+     *
+     * @param binaryString  binary representation of the user input
+     * @return              the chuck norris unary code, created from the binaryString
+     */
+    private static String convertToCNUCode(String binaryString) {
+        StringBuilder sb = new StringBuilder();
+
+        int sameCharsInARow = 0;
+        for (int i = 0; i < binaryString.length(); i++) {
+            sameCharsInARow++;
+
+            if (i + 1 < binaryString.length() && binaryString.charAt(i + 1) != binaryString.charAt(i)
+                    || i + 1 == binaryString.length()) {
+                String oneZero = binaryString.charAt(i) == '1' ? "0 " : "00 ";
+                sb.append(oneZero);
+
+                sb.append("0".repeat(Math.max(0, sameCharsInARow)));
+
+                sb.append(' ');
+                sameCharsInARow = 0;
+            }
+        }
+
+        return sb.toString().trim();
+    }
+
+    /**
+     * Converts chuck norris unary code to binary.
+     *
+     * @param cnuCode  chuck norris code message
+     * @return         binary representation of the converted code
+     */
+    private static String cnuCodeToBinary(String cnuCode) {
+        StringBuilder sb = new StringBuilder();
+
+        int whiteSpaceCounter = 0, startingIndex = 0;
+        for (int i = 0; i < cnuCode.length(); i++) {
+            if (cnuCode.charAt(i) == ' ') {
+                whiteSpaceCounter++;
+            }
+
+            if (whiteSpaceCounter == 2 || i == cnuCode.length() - 1) {
+                String pair = cnuCode.substring(startingIndex, i + 1).trim();
+                startingIndex = i;
+                whiteSpaceCounter = 0;
+
+                String[] pairArr = pair.split(" ");
+                String oneZero = pairArr[0].equals("0") ? "1" : "0";
+                sb.append(oneZero.repeat(pairArr[1].length()));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Converts binary to a string.
+     *
+     * @param binaryString  binary representation of the message to be converted
+     * @return              the decoded message from binary
+     */
+    private static String binaryToString(String binaryString) {
+        ArrayList<String> lettersInBinary = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        // Split at 7.
+        for (int i = 0; i < binaryString.length(); i += 7) {
+            lettersInBinary.add(binaryString.substring(i, i + 7));
+        }
+
+        // Converts the binary to character.
+        for (String letter : lettersInBinary) {
+            sb.append((char) Integer.parseInt(letter, 2));
+        }
+
+        return sb.toString();
+    }
+
+
 }
